80f3b8b66f02c7d1f2b563742e7523d36e309c46
Flax163 <goncharov.valia@yandex.ru>
2016-12-05 01:14:04 +0400

    # improvement routing

--- routing/src/main/ts/administrator/administrator-routing.module.ts
+++ routing/src/main/ts/administrator/administrator-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AdministratorComponent } from "./administrator.component";
+import { AuthGuard } from "./service/auth-guard.service";
+import { LoginComponent } from "./component/login/login.component";
 
 const administratorRoutes:Routes = [
-    {path: 'administrator', component: AdministratorComponent}
+    {path: 'admin', component: AdministratorComponent},
+    {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
     imports: [RouterModule.forChild(administratorRoutes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 
 })
 export class AdministratorRoutingModule {}
--- routing/src/main/ts/administrator/administrator.component.ts
+++ routing/src/main/ts/administrator/administrator.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:"administrator",
+    selector:"admin",
     templateUrl: "administrator.component.html"
 })
 export class AdministratorComponent {
