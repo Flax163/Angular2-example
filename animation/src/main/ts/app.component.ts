ddfff11f4729cdf35ab8622210a8cfaef738d870
Flax163 <goncharov.valia@yandex.ru>
2016-12-08 00:50:37 +0400

    # test animations

--- animation/src/main/ts/app.component.ts
+++ animation/src/main/ts/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, trigger, state, style, transition, animate } from "@angular/core";
 
 @Component({
     selector: 'app-component',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    animations: [
+        trigger('testTrigger', [
+            state('inactive', style({
+                backgroundColor: '#eee',
+                transform: 'scale(1)'
+            })),
+            state('active',   style({
+                backgroundColor: '#cfd8dc',
+                transform: 'scale(1.1)'
+            })),
+            transition('inactive => active', animate('100ms ease-in')),
+            transition('active => inactive', animate('100ms ease-out'))
+        ])
+    ]
 })
 export class AppComponent {
+    state = 'active';
 
+    changeState():void{
+        if (this.state == 'active') {
+            this.state = 'inactive';
+        } else {
+            this.state = 'active';
+        }
+    }
 }
