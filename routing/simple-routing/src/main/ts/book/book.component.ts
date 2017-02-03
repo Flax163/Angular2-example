633f4c307e3487f1af3388992463524eb4b31212
Flax163 <goncharov.valia@yandex.ru>
2017-02-03 23:01:23 +0400

    global refactoring module

--- routing/simple-routing/src/main/ts/book/book.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "book",
-    template: `<h1>Book component</h1>`
-})
-export class BookComponent {}
--- routing/simple-routing/src/main/ts/list-book/book-list.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "list-book",
-    template: `<h1>List book component</h1>`
-})
-export class BookListComponent {}
