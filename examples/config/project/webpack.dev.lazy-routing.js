74e1ad3813618459c75d04e69f4adeeb3ade51dc
Flax163 <goncharov.valia@yandex.ru>
2017-02-03 21:56:55 +0400

    global refactoring module

--- examples/config/project/webpack.dev.lazy-routing.js
+++ examples/config/project/webpack.dev.lazy-routing.js
@@ -10,5 +10,11 @@ module.exports = webpackMerge(webpackDev, {
             'process.env': {
                 'PROJECT': JSON.stringify(PROJECT)
             }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            test: /\.ts$/,
+            options: {
+                loaders: ['angular2-router-loader']
+            }
         })]
 });
