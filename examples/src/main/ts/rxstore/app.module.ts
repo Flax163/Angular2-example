b016a72d09ebbda9e2f01e3a2d42fc12b999086f
Flax163 <goncharov.valia@yandex.ru>
2017-02-08 23:41:38 +0400

    test ngrx-store

--- examples/src/main/ts/rxstore/app.module.ts
+++ examples/src/main/ts/rxstore/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {StoreModule} from "@ngrx/store";
+import { StoreModule } from "@ngrx/store";
+import { CoreLibraryModule } from "../coreLibraryModule/core.library.module";
 
 @NgModule({
-    imports: [BrowserModule, StoreModule.provideStore()],
+    imports: [BrowserModule, CoreLibraryModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
