452e6414be723312edb20c7d5147374726b4bfdb
Flax163 <goncharov.valia@yandex.ru>
2017-02-20 22:01:51 +0400

    refactoring

--- examples/src/main/ts/routing/multiple-outlet/content/content-routing.module.ts
+++ examples/src/main/ts/routing/multiple-outlet/content/content-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { BookComponent } from "./component/book/book.component";
-import { BookListComponent } from "./component/book-list/book.list.component";
 import { ContentComponent } from "./content.component";
+import { ListBookComponent } from "../../../coreLibraryModule/component/listbook/list.book.component";
 
 const contentRouters:Routes = [
     {path: "library", component: ContentComponent,
         children: [
             {path: '', component: BookComponent},
             {path: 'book', component: BookComponent},
-            {path: 'book-list', component: BookListComponent}
+            {path: 'list-book', component: ListBookComponent},
         ]}
 ];
 
--- examples/src/main/ts/routing/multiple-outlet/content/content.module.ts
+++ examples/src/main/ts/routing/multiple-outlet/content/content.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { ContentRoutingModule } from "./content-routing.module";
-import { BookListComponent } from "./component/book-list/book.list.component";
 import { BookComponent } from "./component/book/book.component";
 import { ContentComponent } from "./content.component";
 import { ToolbarModule } from "../toolbar/toolbar.module";
+import { CoreLibraryModule } from "../../../coreLibraryModule/core.library.module";
 
 @NgModule({
-    imports: [ContentRoutingModule, ToolbarModule],
-    declarations: [BookListComponent, BookComponent, ContentComponent]
+    imports: [ContentRoutingModule, ToolbarModule, CoreLibraryModule],
+    declarations: [BookComponent, ContentComponent]
 })
 export class ContentModule {}
